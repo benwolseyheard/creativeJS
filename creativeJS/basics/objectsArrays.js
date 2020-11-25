b4e2963665cedcfd02e89af1ec606b488a533b1c
benwolseyheard <b.wolseyheard@gmail.com>
2020-11-25 12:38:55 +0000

    objects lecture

---
+++ creativeJS/basics/objectsArrays.js
@@ -0,0 +1,18 @@
+/** @format */
+
+"use strict";
+//Objects
+const user = {
+	//Properties --> variables inside of objects
+	//Key-Value pairs
+	name: "Ben",
+	age: 25,
+	married: false,
+	//Methods --> functions inside of objects
+	greet: function () {
+		console.log("Hello there!");
+	},
+};
+
+console.log(user.name);
+user.greet;
