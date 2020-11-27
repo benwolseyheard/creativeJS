7c6ed25cae7de12ec04504bf98bab2a0be0cce1a
benwolseyheard <b.wolseyheard@gmail.com>
2020-11-27 17:07:09 +0000

    scoping lecture

---
+++ creativeJS/basics/scope.js
@@ -0,0 +1,25 @@
+/** @format */
+
+// SCOPE LECTURE
+
+//GLOBAL SCOPE
+let food = "apple";
+let fruits = 5;
+
+//FUNCTION SCOPED
+function store() {
+	let food = "banana";
+	let fruits = 10;
+	console.log(food, fruit);
+}
+
+console.log(food, fruit);
+store();
+
+//BLOCK SCOPE
+//Anything within a block statement { }, new to ES6
+
+//var does not have any block scope so we must use let or const
+for (let fruits = 0; fruits < 10; fruits++) {
+	console.log(fruits);
+}
